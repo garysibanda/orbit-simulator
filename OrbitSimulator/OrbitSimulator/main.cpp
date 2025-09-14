390d58015973e92a507ffdf3cb0dacd541b48b5a
Gary Sibanda <sibandamthandazo@gmail.com>
2025-09-13 19:17:28 -0600

    Add initial Xcode Command Line Tool project

---
+++ OrbitSimulator/OrbitSimulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OrbitSimulator
+//
+//  Created by Gary Sibanda on 9/13/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
